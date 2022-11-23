b8884060abb443cd5b9c5624782fbe1cd40fe395
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2022-11-23 18:23:31 +0100

    -

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+  basePath: "/GreenBoost-LP",
+  assetPrefix: "/GreenBoost-LP",
 }
 
 module.exports = nextConfig
